e63c4e47bb8950f79dc7a7f3cd1d65a5084c8d51
luno3301 <113801241+luno3301@users.noreply.github.com>
2023-10-12 20:31:25 +0300

    Задача 3

---
+++ wawe3.py
@@ -0,0 +1,18 @@
+import wave
+import struct
+
+source = wave.open("in.wav", mode="rb")
+dest = wave.open("out.wav", mode="wb")
+dest.setparams(source.getparams())
+
+frames_count = source.getnframes()
+data = struct.unpack("<" + str(frames_count) + "h", source.readframes(frames_count))
+newdata = []
+for i in range(len(data)):
+    newdata.append(data[i] // 20)
+
+newframes = struct.pack("<" + str(len(newdata)) + "h", *newdata)
+
+dest.writeframes(newframes)
+source.close()
+dest.close()
